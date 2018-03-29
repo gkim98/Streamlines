21de0001aae3710dd23327bf13b518aa1d2bab82
David Cheng <dc273@duke.edu>
2018-03-29 13:37:35 -0400

    Text Field

--- src/components/WritePage.js
+++ src/components/WritePage.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import $ from 'jquery';
 
-const WritePage = () => (
-    <div className="container">
-        This is the page where creative juices will flow
-    </div>
-);
+$(document).keydown(function(e) { 
+    if (e.keyCode == 8 || e.keyCode == 37 || e.keyCode == 39) e.preventDefault(); 
+});
 
-export default WritePage;
+$(document).click(function() { 
+    $(".container__text-field").focus();
+});
+
+
+
+export default class WritePage extends React.Component {
+    componentDidMount() {
+        $(".container__text-field").focus();
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <input type="text" className="container__text-field"/>
+                <div className="container__filter"/> 
+            </div>
+        );
+    }
+};
+    
