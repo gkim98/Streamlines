9c39ff27a3e1b3f8bebf45def75cd0ed10c56256
Grant Kim <grantkim93@gmail.com>
2018-10-24 02:05:45 -0400

    does it work now?

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const config = {
+    apiKey: "AIzaSyDE1j3cwYHtDxtNuk11WPpYoak3PLJ7Rlg",
+    authDomain: "streamlines-bfd08.firebaseapp.com",
+    databaseURL: "https://streamlines-bfd08.firebaseio.com",
+    projectId: "streamlines-bfd08",
+    storageBucket: "streamlines-bfd08.appspot.com",
+    messagingSenderId: "834847952280"
+  };
